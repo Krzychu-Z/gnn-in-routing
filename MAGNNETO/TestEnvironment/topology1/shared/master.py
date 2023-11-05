22f0e3e296d8138de40f6e16f0e710085a3bc9a7
Krzychu-Z <zerebecki.k@gmail.com>
2023-11-05 23:25:39 +0100

    MAGNNETO - global state calculations

--- MAGNNETO/TestEnvironment/topology1/shared/master.py
+++ MAGNNETO/TestEnvironment/topology1/shared/master.py
@@ -12,11 +12,15 @@ MESSAGE_STEPS = 4        # in paper - K
 EPSILON = 0.01
 DISCOUNT = 0.97
 ALPHA = 0.9
+STD_DEV_THRESHOLD = 1e-04
 
 WEB_PREFIX = 'https://'
 CERT_PATH = '/shared/certs/cert'
 
 CURRENT_REWARD = []      # Readout result
+
+# 0 - all routers idle, 2^n - all routers equally load network
+CURRENT_GLOBAL_STATE = 0
 
 
 def single_agent_mp(index):
@@ -78,6 +82,39 @@ response = requests.get(request_string, verify=CERT_PATH + str(1) + ".pem")
 
 edge_list = response.json()
 
+# Compute router-based average egress link utilisation
+avg_util = {}
+
+for router in range(1, router_count + 1):
+    router_util = []
+
+    for edge in edge_list:
+        # Without empty indices
+        indices = [index for index in edge['pair'].split('R') if index != '']
+        # Only edges regarding our current router
+        if str(router) in indices:
+            indices.remove(str(router))
+            egress_id = "to_R" + indices[0] + "_avg"
+            router_util.append(float(edge[egress_id]))
+
+    current_avg = np.average(router_util)
+    avg_util[router] = current_avg
+
+# Calculate standard deviation and average
+avg_list = [value for value in avg_util.values()]
+std_dev = np.std(avg_list)
+load_avg = np.average(avg_list)
+
+# Translate avg_util to global environment state
+if std_dev <= STD_DEV_THRESHOLD:
+    # Here all routers can be treated as balanced
+    CURRENT_GLOBAL_STATE = 2**router_count - 1
+else:
+    # Binary binning
+    for each in range(router_count):
+        if avg_util[each + 1] >= load_avg:
+            CURRENT_GLOBAL_STATE += 2**(router_count - 1 - each)
+
+print(CURRENT_GLOBAL_STATE)
+
 q_value_table = np.zeros((2**len(edge_list), len(edge_list)))
-
-
