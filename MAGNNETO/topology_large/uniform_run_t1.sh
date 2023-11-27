800ad4a5ab20763f2be1ee79c5b0a08e1f86fd0f
Krzychu-Z <zerebecki.k@gmail.com>
2023-11-27 20:01:12 +0100

    Modifying already modified

--- MAGNNETO/topology_large/uniform_run_t1.sh
+++ MAGNNETO/topology_large/uniform_run_t1.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
-kathara exec pc1 "./shared/uniform_storm_t1.sh" &
-kathara exec pc2 "./shared/uniform_storm_t1.sh" &
-kathara exec pc3 "./shared/uniform_storm_t1.sh" &
-kathara exec pc4 "./shared/uniform_storm_t1.sh" &
-kathara exec pc5 "./shared/uniform_storm_t1.sh" &
+kathara exec pc_ams "./shared/uniform_storm_t1.sh" &
+kathara exec pc_bel "./shared/uniform_storm_t1.sh" &
+kathara exec pc_ber "./shared/uniform_storm_t1.sh" &
+kathara exec pc_bil "./shared/uniform_storm_t1.sh" &
+kathara exec pc_bra "./shared/uniform_storm_t1.sh" &
+kathara exec pc_bru "./shared/uniform_storm_t1.sh" &
+kathara exec pc_buc "./shared/uniform_storm_t1.sh" &
+kathara exec pc_bud "./shared/uniform_storm_t1.sh" &
+kathara exec pc_fra "./shared/uniform_storm_t1.sh" &
+kathara exec pc_gen "./shared/uniform_storm_t1.sh" &
+kathara exec pc_ham "./shared/uniform_storm_t1.sh" &
+kathara exec pc_lis "./shared/uniform_storm_t1.sh" &
+kathara exec pc_lju "./shared/uniform_storm_t1.sh" &
+kathara exec pc_lon "./shared/uniform_storm_t1.sh" &
+kathara exec pc_mad "./shared/uniform_storm_t1.sh" &
+kathara exec pc_mar "./shared/uniform_storm_t1.sh" &
+kathara exec pc_mil "./shared/uniform_storm_t1.sh" &
+kathara exec pc_par "./shared/uniform_storm_t1.sh" &
+kathara exec pc_por "./shared/uniform_storm_t1.sh" &
+kathara exec pc_poz "./shared/uniform_storm_t1.sh" &
+kathara exec pc_pra "./shared/uniform_storm_t1.sh" &
+kathara exec pc_sin "./shared/uniform_storm_t1.sh" &
+kathara exec pc_sof "./shared/uniform_storm_t1.sh" &
+kathara exec pc_the "./shared/uniform_storm_t1.sh" &
+kathara exec pc_vie "./shared/uniform_storm_t1.sh" &
+kathara exec pc_zag "./shared/uniform_storm_t1.sh"
